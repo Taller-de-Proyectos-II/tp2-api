277a8066718cb303af4e2aae466ee64fb560c82e
JimenaAlexandraRuizCerna <jruiz@hiper.com.pe>
2021-03-22 22:26:02 -0500

    Estructura inicial
    Estructura inicial del proyecto.

---
+++ src/main/java/com/jyellow/tp2api/Tp2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.jyellow.tp2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyellow/tp2api/Tp2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyellow.tp2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
