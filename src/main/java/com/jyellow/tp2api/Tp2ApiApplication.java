0cc3bb5aebf56f88f7a169a2966e84a61d6378de
JimenaAlexandraRuizCerna <jruiz@hiper.com.pe>
2021-05-23 18:16:29 -0500

    Set timeZone

--- src/main/java/com/jyellow/tp2api/Tp2ApiApplication.java
+++ src/main/java/com/jyellow/tp2api/Tp2ApiApplication.java
@@ -1,11 +1,22 @@
 package com.jyellow.tp2api;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Tp2ApiApplication {
 
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+        System.out.println(new Date());
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Tp2ApiApplication.class, args);
 	}
